9c4a4767c3a76988962340a4bf5ddf36496cf5d4
kellyxglobal <kellyxglobal@gmail.com>
2024-09-06 01:07:44 +0100

    modelling user table with all the specific attributes, migrating th table and creating fake data via seeder

---
+++ library-API/database/seeders/MembersTabeSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MembersTabeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            // Admin
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => HASH::make('123'),
+                'role' => 'Admin',
+            ]
+            ]);
+    }
+}
